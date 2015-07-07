7703ef08c69bbd5cce79e87ad3e899fe6088826c
Elin <elinnet@gmail.com>
2015-07-07 19:14:17 +0100

    add JS to change colorsof background dependant on temp, change temperature figure and place powersave function

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -5,29 +5,56 @@ var temp = document.getElementById("temp");
 
 temp.innerHTML = thermostat.temperature;
 
-var up = element("up-button");
 
-up.onClick = thermostat.increase;
+document.getElementById("up-button").onclick = function(){
+ thermostat.increase();
+ console.log("up");
 
-var down = element("down-button");
+  if (thermostat.temperature === 25 && thermostat._powerSave === true){
+    alert("You have reached the max temp")
+  // };
+  } else if (thermostat.temperature === 32 && thermostat._powerSave === false) {
+    alert("You have reached the max temp")
+  } else {
+    document.getElementById("temp").innerHTML = thermostat.temperature;
+    refresh_display();
+  };
+};
 
-down.onClick = thermostat.decrease();
-
-// function myFunction() {
-//   document.getElementById("temp").innerHTML = thermostat.temperature;
-// }
-
-
-// function increase() {
-//   document.getElementById("temp").innerHTML = thermostat.increase();
-// }
-//
+document.getElementById("down-button").onclick = function(){
+  thermostat.decrease();
+  document.getElementById("temp").innerHTML = thermostat.temperature;
+  console.log("down");
+  refresh_display();
+};
 
 
+function refresh_display(){
+  if (thermostat.temperature > 25){
+    document.getElementById("body").style.background = "red";
+  } else if (thermostat.temperature > 18 && thermostat.temperature < 25) {
+    document.getElementById("body").style.background = "green";
+  } else {
+    document.getElementById("body").style.background = "yellow";
+  };
+};
 
 
-// function myFunction() {
-//   document.getElementById("temp").innerHTML = thermostat.temperature;
-//
-// }
+document.getElementById("power-save").onclick = function(){
+  if (document.getElementById("power-save").classList == "ON"){
+        
+        document.getElementById("power-save").classList.remove("ON");
+        document.getElementById("power-save").classList.add("OFF");
+        thermostat._powerSave = false;
+  
+  } else {
+        document.getElementById("power-save").classList.remove("OFF");
+        document.getElementById("power-save").classList.add("ON");
+        thermostat._powerSave = true;
+
+  }
+};
+
+
+
 
